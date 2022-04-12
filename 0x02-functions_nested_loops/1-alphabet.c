4a4b26a52be67451c2ad1aa217ccacf345d453a6
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-12 14:54:26 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,15 @@
 
 /**
  * main - check the code
- *print_alphabet-print alphabet lowercase
- *
- * Return: Always 0.
+ * print_alphabet-print alphabet in lowercase, followed by a new line.
  */
+void print_alphabet(void);
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar('letter')
+_putchar('\n')
+
+return (0)
+}
