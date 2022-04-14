bdc803eb9b5310d98fe01f4f201f5b7b760840a1
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-14 13:35:49 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+{
+printf("%i", i);
+}
+
+printf("\n");
+return (0);
+}
