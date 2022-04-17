ee91fe1514ff9c2a26d1c17ccdb13f3508fa654c
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-17 11:06:16 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
 /**
  * print_numbers - print numbers
- *
  * Return: Always 0
  */
-
 void print_numbers(void)
 {
-
 int i;
 
 i = 0;
-
-for (i = 0; i < 10; i++)
-
+while (i < 10)
+{
 _putchar(i + '0');
+i++;
+}
 
 _putchar('\n');
 }
