b670a705c5fbc664520859764b5d094f36b9d2c8
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-20 11:41:25 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates a integer value to 98
+ * @n: a pointer to an int that will be updated
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
+
