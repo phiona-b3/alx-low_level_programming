be6c6c510d0ebc47773765d9d260577a2bf0032c
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-21 12:06:49 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,6 +4,10 @@
   * @n: number
   * Return: if is a number, return 1, else return 0
   */
+int is_numerical(unsigned int n)
+{
+return (n >= '0' && n <= '9');
+}
 
 /**
  * _atoi -  convert a string to an integer
