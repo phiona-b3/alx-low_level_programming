759b08af0cae2b35a46005140c10273900829e2e
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-26 01:55:58 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,9 @@ int i;
 
 for (i = 0; str[i] != '\0', i++)
 {
-if (i == 0)
+if (i = 0)
 {
-if((str[i] >= 'a' && str[i] <= 'z'))
+if (str[i] >= 'a' && str[i] <= 'z')
 str[i] = str[i] - 32;
 continue;
 }
