a52913d221435716388daccea7ca3e790b34b9d4
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-29 08:58:14 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *@n: integer
+ *i: integer
+ *Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+int i = 0;
+
+if (i < 0)
+return (-1);
+
+else
+return (i, n);
+}
+
+
