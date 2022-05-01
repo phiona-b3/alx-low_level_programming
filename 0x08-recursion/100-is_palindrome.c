a17dfcd8268b6b748633b4766d53d91d5268e4af
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-04-30 20:44:55 -0700

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - size
+ *@s: pointer to a string params
+ *Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+if (!*s)
+{
+return (0);
+}
+return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer to a string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+if (l < 1)
+{
+return (1);
+}
+
+if (*s == (*s + l))
+{
+return (p1(s + 1, l - 2));
+}
+return (0);
+}
