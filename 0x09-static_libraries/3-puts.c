e317eb09493c20cc2b24a01aee63b5bc5d95bb62
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-05-03 12:16:12 -0700

    Tasks

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: a pointer to an int that will be changed
+ * 
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+char *c;
+int m;
+
+c =  str;
+
+for (m = 0; c[m]; m++)
+{
+_putchar (c[m]);
+}
+_putchar('\n');
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s: string
+ * @accept: character in str1 that matches one of the characters in str2
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int j;
+
+while (*s != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (*s == accept[j])
+{
+return (s);
+}
+
+j++;
+}
+
+s++;
+}
+return (0);
+
+}
