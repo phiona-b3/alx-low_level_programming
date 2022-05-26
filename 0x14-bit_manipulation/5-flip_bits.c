9902e4fe5017598c4f421c77854ffed6afac101e
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-05-26 13:54:15 -0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits -  number of bits you would need to flip
+ * to get from one number to another
+ *@n: first number
+ *@m: second number
+ *
+ *Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+int i, count = 0;
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
+
+for (i = 63; i >= 0; i--)
+{
+current = exclusive >> i;
+if (current & i)
+count++;
+}
+
+return (count);
+}
