9208172415199f0a3c6fbe6f8fc0618df1f7b5a6
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-16 12:56:26 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,7 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "main.h"
 /**
  * print_numbers - print numbers
@@ -10,10 +14,10 @@ int i;
 i = 0;
 while (i < 10)
 {
-_putchar(i);
+putchar(i + '0');
 i++;
 }
 
-_putchar('\n');
+putchar('\n');
 }
 
