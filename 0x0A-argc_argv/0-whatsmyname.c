5f2cbc0c0b580ea95bc60a89e5e51b5e046b42ec
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-16 10:45:38 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument variable
+ *
+ * Return:0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s/n", argv[0]);
+
+return (0);
+}
