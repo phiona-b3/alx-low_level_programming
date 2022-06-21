a6e86f4d45835e2d638695bf27ecb0c264ae9a7d
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-21 13:33:20 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%i\n", argc - 1);
+
+return (0);
+}
