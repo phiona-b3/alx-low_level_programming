25f7999999aecaa5d2859ca3c4cd84a6639ee1d5
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-21 13:17:24 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * main - prints the program name
  * @argc: argument count
- * @argv: argument variable
+ * @argv: arguments
  *
- * Return:0 on success
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
 (void) argc;
-printf("%s/n", argv[0]);
+printf("%s\n", argv[0]);
 
 return (0);
 }
