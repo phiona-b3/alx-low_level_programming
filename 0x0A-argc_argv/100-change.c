2765fb9eb083ad921c5fa41d64e6378f9a77dc84
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-21 19:59:06 -0700

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+int position, total, change, aux;
+int coins[] = {25, 10, 5, 2, 1}; /*array int*/
+
+position = total = change = aux = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+total = atoi(argv[1]);
+
+if (total <= 0)
+{
+printf("0\n");
+return (0);
+}
+
+while (coins[position] != '\0')
+{
+if (total >= coins[position])
+{
+aux = (total / coins[position]);
+change += aux;
+total -= coins[position] * aux;
+}
+position++;
+}
+
+printf("%d\n", change);
+return (0);
+}
