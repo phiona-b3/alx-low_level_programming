61b1822e65033a38d5cef7db417397731817b472
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-21 19:13:45 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+int a;
+int b;
+if (argc == 3)
+{
+printf("%d\n", a * b);
+return (0);
+}
+
+printf("Error\n");
+return (1);
+}
