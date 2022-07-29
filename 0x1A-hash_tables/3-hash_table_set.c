d6d70c7dac436dc778de5ce812f6ceddcd625655
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-29 10:58:33 -0700

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - insert a key, value pair to a hash table.
+ * @ht: hash table to insert to.
+ * @key: key
+ * @value: value to be stored at that key.
+ * Return: 1 on SUCCESS otherwise 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+unsigned long int idx;
+char *str;
+hash_node_t *node;
+if (!ht || !key)
+return (0);
+idx = key_index((unsigned char *)key, ht->size);
+if (value)
+str = strdup(value);
+else
+str = NULL;
+node = ht->array[idx];
+if (!node)
+node = new_node(NULL);
+if (node->key)
+{
+ht->array[idx] = update_node(node, key, str);
+return (1);
+node->key = strdup(key)
+node->value = str;
+node->next = NULL;
+ht->array[idx] = node;
+return (1);
+}
