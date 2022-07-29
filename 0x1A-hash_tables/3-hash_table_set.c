1515965d5f728e004bdaab601de0309f01f02638
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-29 12:56:04 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int idx;
 char *str;
 hash_node_t *node;
+
 if (!ht || !key)
 return (0);
 idx = key_index((unsigned char *)key, ht->size);
@@ -21,8 +22,10 @@ str = strdup(value);
 else
 str = NULL;
 node = ht->array[idx];
+
 if (!node)
 node = new_node(NULL);
+
 if (node->key)
 {
 ht->array[idx] = update_node(node, key, str);
@@ -31,6 +34,7 @@ return (1);
 node->key = strdup(key);
 node->value = str;
 node->next = NULL;
+
 ht->array[idx] = node;
 return (1);
 }
@@ -47,7 +51,8 @@ hash_node_t *update_node(hash_node_t *node, const char *key, char *value)
 hash_node_t *tmp = node;
 
 while (tmp)
-					{
+{
+
 if (!strcmp(key, tmp->key))
 {
 free(tmp->value);
@@ -67,6 +72,7 @@ return (node);
 node->key = strdup(key);
 node->value = value;
 return (node);
+
 }
 
 /**
