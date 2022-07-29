0b6492fdcc12f95a647641dfb504e32d93fe34a1
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-29 12:43:25 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "hash_tables.h"
 
 /**
@@ -24,7 +25,7 @@ if (!node)
 node = new_node(NULL);
 if (node->key)
 {
-iht->array[idx] = update_node(node, key, str);
+ht->array[idx] = update_node(node, key, str);
 return (1);
 }
 node->key = strdup(key);
@@ -32,4 +33,58 @@ node->value = str;
 node->next = NULL;
 ht->array[idx] = node;
 return (1);
+}
+
+/**
+ * update_node - update or added a node if one already exists
+ * @node: pointer to a node the exist
+ * @key: key
+ * @value: value to update/create node with
+ * Return: pointer to head of a list
+ */
+hash_node_t *update_node(hash_node_t *node, const char *key, char *value)
+{
+hash_node_t *tmp = node;
+
+while (tmp)
+					{
+if (!strcmp(key, tmp->key))
+{
+free(tmp->value);
+tmp->value = value;
+return (node);
+}
+tmp = tmp->next;
+}
+
+tmp = node;
+node = new_node(node);
+if (!node)
+{
+node = tmp;
+return (node);
+}
+node->key = strdup(key);
+node->value = value;
+return (node);
+}
+
+/**
+ * new_node - create a new node
+ * @c_node: current node to add add at the end.
+ *
+ * Return: a point to the new node.
+ */
+hash_node_t *new_node(hash_node_t *c_node)
+{
+hash_node_t *node;
+
+node = malloc(sizeof(hash_node_t));
+if (!node)
+return (NULL);
+node->key = NULL;
+node->value = NULL;
+node->next = c_node;
+
+return (node);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -42,4 +42,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
+hash_node_t *update_node(hash_node_t *node, const char *key, char *value);
+hash_node_t *new_node(hash_node_t *);
 #endif
